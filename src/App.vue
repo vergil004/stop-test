ae7860c3951f435de5a6a72c457d0e882da614b2
vergil004 <vergil004@gmail.com>
2023-08-21 13:57:26 +0500

    stop-test: первый коммит

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <payments-app></payments-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PaymentsApp from "@/components/PaymentsApp";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PaymentsApp
   }
 }
 </script>
---
+++ src/components/PaymentsApp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Список платежей</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
